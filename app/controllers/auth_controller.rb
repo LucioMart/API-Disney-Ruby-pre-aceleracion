4a9dda93a78bc9343ef32ceb24cbf141a538fe5b
LucioMart <luchoweb15@gmail.com>
2021-06-06 12:47:53 -0300

    definicion del metodo persist en el auth_controllen que toma el token, si existe, para encontrar la instancia de usuario a la que pertenece

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,4 +1,5 @@
 class AuthController < ApplicationController
+  
   def login
     user = User.find_by(username: login_params[:username])
     if user && user.authenticate(login_params[:password])
@@ -10,6 +11,12 @@ class AuthController < ApplicationController
   end
 
   def persist
+    if request.headers['Authorization']
+      encoded_token = request.headers['Authorization'].split(' ')[1]
+      token = JWT.decode(encoded_token, secret)
+      user_id = token[0]['user_id']
+      user = User.find(user_id)
+      render json: user
   end
 
   private
