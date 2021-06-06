e920f8d42ee43258ec396a3cc520a6cc98832163
LucioMart <luchoweb15@gmail.com>
2021-06-06 10:34:04 -0300

    añadido el metodo create en el modelo user, para la creacion de un usuario nuevo con su token para el ingreso y su posterior renderizado

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,15 @@ class UsersController < ApplicationController
         render json: user
     end
 
-   
+    def create
+        user = User.create(user_params)
+        if user.valid?
+           user = user
+           token = JWT.encode({user_id: user.id}, secret, 'HS256')
+           render json: {user: user, token: token}
+        else
+           render json: {errors: user.errors.full_messages}
+        end
+    end
 
 end
