362247368971c03b4575c426749c8b6efbc88f92
LucioMart <luchoweb15@gmail.com>
2021-06-06 12:34:29 -0300

    definicion del metodo login en el auth_controller

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,6 +1,12 @@
 class AuthController < ApplicationController
   def login
     user = User.find_by(username: login_params[:username])
+    if user && user.authenticate(login_params[:password])
+      token = JWT.encode({user_id: user_id}, secret, 'HS256')
+      render json: {user: user, token: token}
+    else
+      render json: {errors: user.errors.full_messages}
+    end
   end
 
   def persist
